d00dbebbed5401001ea65d3964c83da44944d0d2
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-08-25 13:48:02 -0700

    Finished NameTag

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-Bad.code
+
+const nameElement = document.getElementById('name');
+
+const updateButton = document.getElementById('update-button');
+
+const nameInput = document.getElementById('name-input');
+
+updateButton.addEventListener('click', () => {
+console.log('hello')
+    const name = nameInput.value;
+
+    nameElement.textContent = name;
+
+
+
+});
+
